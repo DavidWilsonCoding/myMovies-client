ec1cb89e71260dc0a7309f29950b8868582cd58c
David Wilson <david.wilson.coding@gmail.com>
2022-04-02 06:42:18 +0100

    Added Login and Registration Views

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
-    //onMovieClick and movieData passed from main-view.jsx
     const { movieData, onMovieClick } = this.props;
-    //add onClick event listener for each item of movieData to be returned, return contents of div (will change value of selectedMovie and component state in main-view.jsx)
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
-      {movieData.Title}
-    </div>;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movieData.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movieData)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
-
-MovieCard.propTypes = {
-  movieData: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired
-    }),
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
